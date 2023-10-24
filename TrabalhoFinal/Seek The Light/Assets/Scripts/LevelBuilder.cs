338eda46c8463d960d21d4ff4b46ddedb25ac80c
Gustavo Machado <gustavomf02@gmail.com>
2023-10-24 16:51:20 -0300

    Implementa greedy meshing algorithm

--- TrabalhoFinal/Seek The Light/Assets/Scripts/LevelBuilder.cs
+++ TrabalhoFinal/Seek The Light/Assets/Scripts/LevelBuilder.cs
@@ -12,18 +12,24 @@ public class LevelBuilder : MonoBehaviour
     [SerializeField] private GameObject floor;
 
     [SerializeField] private GameObject wallsParent;
-    [SerializeField] private Material wallMaterial;
+    [SerializeField] private GameObject wallPrefab;
 
     [SerializeField] private GameObject enemiesParent;
-
     [SerializeField] private GameObject enemyPrefab;
+
+    private Vector2[,] cellSizes;
+    private Color[] pixels;
+
+    private int levelWidth;
+    private int levelHeight;
     
     enum LevelElementID
     {
         Wall,
         PowerUp,
         Enemy,
-        Player
+        Player,
+        Path
     }
 
     Dictionary<Color, LevelElementID> levelElementIDByColor = new()
@@ -31,37 +37,50 @@ public class LevelBuilder : MonoBehaviour
         { Color.black, LevelElementID.Wall },
         { Color.blue, LevelElementID.PowerUp },
         { Color.red, LevelElementID.Enemy },
-        { Color.green, LevelElementID.Player }
+        { Color.green, LevelElementID.Player },
+        { Color.white, LevelElementID.Path }
     };
 
     public void BuildLevel()
     {
-        int height = levelMap.height;
-        int width = levelMap.width;
+        levelHeight = levelMap.height;
+        levelWidth = levelMap.width;
 
-        floor.transform.localScale = new Vector3(width, 0.1f, height);
-        floor.transform.position = new Vector3(width / 2, 0, height / 2);
-
-        Color[] pixels = levelMap.GetPixels();
-
-        bool[,] visited = new bool[width, height];
-
-        for (int y = 0; y < height; y++)
+        cellSizes = new Vector2[levelWidth, levelHeight];
+        for (int i = 0; i < levelWidth; i++)
         {
-            for (int x = 0; x < width; x++)
+            for (int j = 0; j < levelHeight; j++)
             {
-                if (visited[x, y])
-                {
-                    continue;
-                }
+                cellSizes[i, j] = Vector2.one;
+            }
+        }
 
-                Color32 pixelColor = pixels[y * width + x];
+        floor.transform.localScale = new Vector3(levelWidth, 0.1f, levelHeight);
+        floor.transform.position = new Vector3(levelWidth / 2, 0, levelHeight / 2);
+
+        pixels = levelMap.GetPixels();
+
+        GroupHorizontalCells();
+        GroupVerticalCells();
+        
+        for (int y = 0; y < levelHeight; y++)
+        {
+            for (int x = 0; x < levelWidth; x++)
+            {
+                if (cellSizes[x, y] == Vector2.zero)
+                    continue;
+
+                Color32 pixelColor = pixels[y * levelWidth + x];
                 LevelElementID elementID = levelElementIDByColor[pixelColor];
 
                 switch (elementID)
                 {
                     case LevelElementID.Wall:
-                        CreateCustomWallMesh(x, y, visited);
+                        GameObject wall = Instantiate(wallPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                        Vector2 size = cellSizes[x, y];
+                        wall.transform.parent = wallsParent.transform;
+                        wall.transform.localScale = new Vector3(size.x, 1, size.y);
+                        wall.transform.position = new Vector3(x - size.x * 0.5f, 0, y - size.y * 0.5f);
                         break;
                     case LevelElementID.PowerUp:
                         // Add power-up
@@ -73,6 +92,8 @@ public class LevelBuilder : MonoBehaviour
                     case LevelElementID.Player:
                         GameManager.Player.SetPosition(new Vector3(x, 1, y));
                         break;
+                    case LevelElementID.Path:
+                        break;
                     default:
                         throw new NotImplementedException();
                 }
@@ -80,15 +101,35 @@ public class LevelBuilder : MonoBehaviour
         }
     }
 
-    private void CreateCustomWallMesh(int startX, int startY, bool[,] visited)
+    private void GroupHorizontalCells()
     {
-
-
-        for (int u = startX; u < levelMap.width; u++)
+        for (int y = 0; y < levelHeight; y++)
         {
-            for (int v = startY; v < levelMap.height; v++)
+            for (int x = 1; x < levelWidth; x++)
             {
-                
+                if (pixels[y * levelWidth + x] != pixels[y * levelWidth + x - 1])
+                    continue;
+                cellSizes[x, y].x += cellSizes[x - 1, y].x;
+                cellSizes[x - 1, y] = Vector2.zero;
+            }
+        }
+    }
+
+    private void GroupVerticalCells()
+    {
+        for (int x = 1; x < levelWidth; x++)
+        {
+            for (int y = 1; y < levelHeight; y++)
+            {
+                if (cellSizes[x, y - 1] == Vector2.zero
+                    || pixels[y * levelWidth + x] != pixels[(y - 1) * levelWidth + x]
+                    || cellSizes[x, y - 1].x != cellSizes[x, y].x)
+                {
+                    continue;
+                }
+
+                cellSizes[x, y].y += cellSizes[x, y - 1].y;
+                cellSizes[x, y - 1] = Vector2.zero;
             }
         }
     }
